3755420a7275937f5ec41f1bd44439cf1222ed0f
EC2 Default User <ec2-user@ip-172-31-47-250.ap-northeast-1.compute.internal>
2023-02-15 03:12:36 +0000

    [Add]フローチャート学習

---
+++ readlines_practice.rb
@@ -0,0 +1,16 @@
+# lines = readlines
+# lines.each do |words|
+#   p words.chomp.split(' ')
+# end
+
+# lines = readlines
+# lines.each do |words|
+#   p words.split # 改行文字も分割するために使用されるため、chompは不要
+# end
+
+lines = readlines
+lines.each do |words|
+  words.chomp.split(' ').each do |word|
+    p word
+  end
+end
